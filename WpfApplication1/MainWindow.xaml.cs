c95458354d8c774554f9e024d3b38272977b3fbf
Sam Greaves <sam.greaves@meyertech.co.uk>
2016-08-02 09:49:46 +0100

    initial commit

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : INotifyPropertyChanged
+    {
+        private bool _isPaused;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            mePlayer.Play();
+            IsPaused = false;
+        }
+
+        private void Play_OnClick(object sender, RoutedEventArgs e)
+        {
+            mePlayer.Play();
+
+            IsPaused = false;
+        }
+
+        private void Pause_OnClick(object sender, RoutedEventArgs e)
+        {
+            mePlayer.Pause();
+
+            IsPaused = true;
+        }
+
+        public bool IsPaused
+        {
+            get { return _isPaused; }
+            set
+            {
+                _isPaused = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
